4349c0016e3463ee723918c57cec73b9dbefe887
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2017-02-14 17:48:55 +0100

    Add makefile, Voigt profile. Move some functions to Testing namespace. Compare two level system with explicit formula.

--- src/twoLevel.cpp
+++ src/twoLevel.cpp
@@ -3,15 +3,19 @@
 #include "NumUtils.h"
 #include "SpecialFunctions.h"
 
+#include <exception>
 #include <vector>
 
 namespace {
 	const double SQRT2PI = 2.50662827463;
+	const double csquare_twoh = Constant::LIGHT * Constant::LIGHT / 2. / Constant::PLANCK;
 }
 
 TwoLevel::TwoLevel(double n, const std::vector<double>& wavelength, const std::vector<double>& isrf)
 	: _n(n), _isrf(isrf), _wavelength(wavelength), _T(0)
 {
+	if (_isrf.size() != _wavelength.size()) throw std::range_error("Given ISRF and wavelength vectors do not have the same size");
+
 	// toy model of CII 158 um from https://www.astro.umd.edu/~jph/N-level.pdf bottom of page 4
 	// this makes collisional deexcitation important for densities > 20-50 / cm3
 	// Level and transition information
@@ -27,64 +31,50 @@ TwoLevel::TwoLevel(double n, const std::vector<double>& wavelength, const std::v
 void TwoLevel::doLevels(double T)
 {
 	_T = T;
+	cout << "Calculating level balance for T = "<<T<<" K"<<endl;
 
-	// Calculate Pij = integral phi * I_lambda
-	for (size_t i = 0; i < _Pvv.rows(); i++)
-	{
-		for(size_t j = i + 1; j < _Pvv.cols(); j++)
-		{
-			Eigen::ArrayXd phi = lineProfile(i, j);
-			std::vector<double> integrand;
-			integrand.reserve(_isrf.size());
-			for (size_t n = 0; n < integrand.size(); n++) integrand.push_back(phi(n) * _isrf[n]);
-			_Pvv(i, j) = NumUtils::integrate<double>(_wavelength, integrand);
-
-			// Copy to the lower triangle
-			_Pvv(j, i) = _Pvv(i, j);
-		}
-		// Set the diagonal to zero
-		_Pvv(i, i) = 0;
-	}
+	prepareAbsorptionMatrix();
 
 	// Calculate Cij
 	prepareCollisionMatrix();
 
-	cout << "Aij" << endl << _Avv << endl;
-	cout << "Pij" << endl << _Pvv << endl;
-	cout << "Cij" << endl << _Cvv << endl;
+	cout << "Aij" << endl << _Avv << endl << endl;
+	cout << "BPij" << endl << _BPvv << endl << endl;
+	cout << "Cij" << endl << _Cvv << endl << endl;
 
 	// Calculate Fij and bi and solve F.n = b
 	solveRateEquations(Eigen::Vector2d::Zero(2), 0);
 }
 
-double TwoLevel::bolometricEmission() const
+double TwoLevel::bolometricEmission(size_t upper, size_t lower) const
 {
-	return (_Ev(1) - _Ev(0)) * Constant::FPI * _nv(1) * _Avv(1,0);
+	return (_Ev(upper) - _Ev(lower)) * Constant::FPI * _nv(upper) * _Avv(1,0);
 }
 
 std::vector<double> TwoLevel::calculateEmission() const
 {
 	// There is only one line for now
-	double lineIntensity = (_Ev(1) - _Ev(0)) / Constant::FPI * _nv(1) * _Avv(1,0);
-	Eigen::ArrayXd result = lineIntensity * lineProfile(0, 1);
-	return std::vector<double>(result.data(), result.data() + result.size());
+	double lineIntensity = bolometricEmission(1, 0);
+	Eigen::ArrayXd result = lineIntensity * lineProfile(1, 0);
+	std::vector<double> resultv(result.data(), result.data() + result.size());
+	return resultv;
 }
 
 std::vector<double> TwoLevel::calculateOpacity() const
 {
 	double nu_ij = (_Ev(1) - _Ev(0)) / Constant::PLANCK;
 	double constantFactor = Constant::LIGHT*Constant::LIGHT / 8. / Constant::PI / nu_ij*nu_ij * _Avv(1,0);
-	double densityFactor = _nv(0)*_gv(1)/_gv(0) - _nv(0);
-	Eigen::ArrayXd result = constantFactor * densityFactor * lineProfile(0, 1);
+	double densityFactor = _nv(0)*_gv(1)/_gv(0) - _nv(1);
+	Eigen::ArrayXd result = constantFactor * densityFactor * lineProfile(1, 0);
 	return std::vector<double>(result.data(), result.data() + result.size());
 }
 
-Eigen::ArrayXd TwoLevel::lineProfile(size_t i, size_t j) const
+Eigen::ArrayXd TwoLevel::lineProfile(size_t upper, size_t lower) const
 {
-	double nu0 = (_Ev(i) - _Ev(j)) / Constant::PLANCK;
+	double nu0 = (_Ev(upper) - _Ev(lower)) / Constant::PLANCK;
 
-	double decayRate = _Avv(1,0) + _Cvv(1,0) // decay rate of top level
-			+ _Cvv(0,1); // decay rate of bottom level
+	double decayRate = _Avv(upper, lower) + _Cvv(upper, lower) // decay rate of top level
+			+ _Cvv(lower, upper); // decay rate of bottom level
 
 	double thermalVelocity = std::sqrt(Constant::BOLTZMAN * _T / Constant::HMASS_CGS);
 
@@ -93,6 +83,7 @@ Eigen::ArrayXd TwoLevel::lineProfile(size_t i, size_t j) const
 
 	// The standard deviation in frequency units. It is about half of the FWHM for a Gaussian
 	double sigma_nu = nu0 * thermalVelocity / Constant::LIGHT;
+	double one_sqrt2sigma = M_SQRT1_2 / sigma_nu;
 
 	// Get an order of magnitude guess of the total width: Gamma / 2 + sigma_nu
 	double sumWidths = halfWidth + sigma_nu;
@@ -118,11 +109,41 @@ Eigen::ArrayXd TwoLevel::lineProfile(size_t i, size_t j) const
 //		// When both components are important, use this approximation of the Voigt profile
 		else
 		{
-			double one_sqrt2sigma = M_SQRT1_2 / sigma_nu;
 			profile(n) = SpecialFunctions::voigt(one_sqrt2sigma * halfWidth, one_sqrt2sigma * deltaNu) / SQRT2PI / sigma_nu;
+			// phi_lambda dlambda = -phi_nu dnu
+			// phi_lambda = -phi_nu * d(c / lambda)/dlambda = phi_nu * c / lambda^2
+			profile(n) *= Constant::LIGHT / _wavelength[n] / _wavelength[n];
 		}
 	}
+	cout << "line profile norm = " << NumUtils::integrate<double>(_wavelength, std::vector<double>(profile.data(), profile.data() + profile.size())) << endl;
 	return profile;
+}
+
+void TwoLevel::prepareAbsorptionMatrix()
+{
+	// Calculate product of Bij and Pij = integral(phi * I_lambda)
+	for (int i = 0; i < _BPvv.rows(); i++)
+	{
+		// Go over the lower triangle, without diagonal
+		for(int j = 0; j < i; j++)
+		{
+			// Calculate Pij for the lower triangle (= stimulated emission)
+			Eigen::ArrayXd phi = lineProfile(i, j);
+			std::vector<double> integrand;
+			integrand.reserve(_isrf.size());
+			for (size_t n = 0; n < _isrf.size(); n++) integrand.push_back(phi(n) * _isrf[n]);
+			_BPvv(i, j) = NumUtils::integrate<double>(_wavelength, integrand);
+
+			// Multiply by Bij in terms of Aij, valid for i > j
+			double nu_ij = (_Ev(i) - _Ev(j)) / Constant::PLANCK;
+			_BPvv(i, j) *= csquare_twoh / nu_ij / nu_ij / nu_ij * _Avv(i,j);
+
+			// Derive the upper triangle (= absorption) using gi Bij = gj Bji and Pij = Pji
+			_BPvv(j, i) = _gv(i) / _gv(j) * _BPvv(i,j);
+		}
+		// Set the diagonal to zero
+		_BPvv(i, i) = 0;
+	}
 }
 
 void TwoLevel::prepareCollisionMatrix()
@@ -144,28 +165,14 @@ void TwoLevel::prepareCollisionMatrix()
 
 void TwoLevel::solveRateEquations(Eigen::Vector2d ne_np_alpha, size_t chooseConsvEq)
 {
-	const double csquare_twoh = Constant::LIGHT * Constant::LIGHT / 2. / Constant::PLANCK;
-
-	// Initialize Mij as Aji + Cji
-	Eigen::MatrixXd Mvv(_Avv.transpose() + _Cvv.transpose());
-
-	// Add BjiPji (loop over the upper triangle j > i of M(i,j) (= lower triangle of B(j,i)) (no diagonal elements, )
-	for (size_t i = 0; i < Mvv.rows(); i++)
-	{
-		for(size_t j = i + 1; j < Mvv.cols(); j++)
-		{
-			double nu_ji = (_Ev(j) - _Ev(i)) / Constant::PLANCK;
-
-			// Expression for Bji (firstindex > secondindex)
-			double Bji = csquare_twoh / nu_ji / nu_ji / nu_ji * _Avv(j,i);
-			Mvv(i,j) += Bji * _Pvv(j,i);
-
-			// For Bij (firstindex < secondindex)
-			Mvv(j,i) += _gv(j) / _gv(i) * Bji * _Pvv(i,j);
-		}
-	}
+	// Initialize Mij as Aji + PBji + Cji
+	// = arrival rate in level i from level j
+	Eigen::MatrixXd Mvv(_Avv.transpose() + _BPvv.transpose() + _Cvv.transpose());
 
 	// See equation for Fij (37) in document
+	// = subtract departure rate from level i to all other levels
+	Eigen::MatrixXd dep = Mvv.colwise().sum().asDiagonal();
+	cout << "dep" << endl << dep << endl << endl;
 	Mvv -= Mvv.colwise().sum().asDiagonal();
 	Eigen::VectorXd b(-ne_np_alpha);
 
@@ -178,6 +185,10 @@ void TwoLevel::solveRateEquations(Eigen::Vector2d ne_np_alpha, size_t chooseCons
 	_nv = Mvv.colPivHouseholderQr().solve(b);
 
 	cout << "nv" << endl << _nv << endl;
+	cout << "from matrix equation nu / nl: " << _nv(1) / _nv(0) << endl;
+
+	//cout << "from explicit formula nu / nl: " << _gv(1) / _gv(0) * exp((_Ev(0) - _Ev(1)) / _T / Constant::BOLTZMAN) / (1 + _Avv(1,0) / _Cvv(1,0)) << endl;
+	cout << "from explicit formula nu / nl: " << (_Cvv(0,1) + _BPvv(0,1)) / (_Avv(1,0) + _Cvv(1,0) + _BPvv(1,0)) << endl;
 }
 
 
