1cc1c7a8d293f363f646ecb041daeb78b6cf3540
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2017-06-07 17:47:07 -0400

    Properly combine the Einstein coefficients (ad-hoc for now).

--- src/HydrogenLevels.cpp
+++ src/HydrogenLevels.cpp
@@ -39,14 +39,7 @@ HydrogenLevels::HydrogenLevels(const Array& frequencyv)
 	DEBUG("Constructed HydrogenLevels" << endl);
 }
 
-int HydrogenLevels::makeNLv() const
-{
-	// Using NIST data
-	// (http://physics.nist.gov/cgi-bin/ASD/lines1.pl?unit=1&line_out=0&bibrefs=1&show_obs_wl=1&show_calc_wl=1&A_out=0&intens_out=1&allowed_out=1&forbid_out=1&conf_out=1&term_out=1&enrg_out=1&J_out=1&g_out=0&spectra=H%20I)
-	// 1s, 2p, 2s
-	// 3, 4, 5
-	return NLV;
-}
+int HydrogenLevels::makeNLv() const { return NLV; }
 
 Eigen::VectorXd HydrogenLevels::makeEv() const
 {
@@ -65,25 +58,61 @@ Eigen::VectorXd HydrogenLevels::makeGv() const
 
 Eigen::MatrixXd HydrogenLevels::makeAvv() const
 {
+	// Using NIST data
+	// (http://physics.nist.gov/cgi-bin/ASD/lines1.pl?unit=1&line_out=0&bibrefs=1&show_obs_wl=1&show_calc_wl=1&A_out=0&intens_out=1&allowed_out=1&forbid_out=1&conf_out=1&term_out=1&enrg_out=1&J_out=1&g_out=0&spectra=H%20I)
+	// 1s, 2p, 2s
+	// 3, 4, 5
+	// The l-resolved data are also j-resolved however, so we'll have to average over initial
+	// and sum over lower states
+
+	// Decays to 1
 	double A2p1 = 6.2649e8;
 	double A2s1 = 2.495e-6;
-
 	double A31 = 5.5751e7;
-	double A32 = 4.4101e+07;
-	double A32p = 3 * A32 / 4.;
-	double A32s = A32 / 4.;
-
 	double A41 = 1.2785e+07;
-	double A42 = 8.4193e+06;
-	double A42p = 3 * A42 / 4.;
-	double A42s = A42 / 4.;
-	double A43 = 8.9860e+06;
-
 	double A51 = 4.1250e+06;
+
+	// Decays to 2
+	// l has to change by +-1, so 3 --> 2s can only happen from 3p
+	// Two different initial states --> average
+	double A3p2s = (2 * 2.2449e+07 + 4 * 2.2448e+07) / 6.;
+	double A32s = (1 * 0. + 3 * A3p2s + 5 * 0.) / 9.;
+
+	// Two different final states --> sum
+	double A3s2p = 4.2097e+06 + 2.1046e+06;
+	// Different final states and initial states --> both sum and average
+	double A3d2p = (4 * 1.0775e+07 + 6 * 6.4651e+07 + 4 * 5.3877e+07 ) / 10.;
+	double A32p = (1 * A3s2p + 3 * 0. + 5 * A3d2p) / 9.;
+
+	// Keep this as a reference. The sum of A32s and A32p should equal this.
+	double A32 = 4.4101e+07;
+	printf("A32 = %e, A32p + A32s = %e\n", A32, A32p + A32s);
+
+	double A4p2s = (2 * 9.6683e+06 + 4 * 9.6680e+06) / 6.;
+	double A42s = 3 * A4p2s / 16.;
+
+	double A4s2p = 1.7190e+06 + 8.5941e+05;
+	double A4d2p = (4 * 3.4375e+06 + 6 * 2.0625e+07 + 4 * 1.7188e+07) / 10.;
+	double A42p = (1 * A4s2p + 3 * 0. + 5 * A4d2p + 7 * 0.) / 16.;
+
+	double A42 = 8.4193e+06;
+	printf("A42 = %e, A42p + A42s = %e\n", A42, A42p + A42s);
+
+	double A5p2s = (2 * 4.9484e+06 + 4 * 4.9483e+06) / 6.;
+	double A52s = 3 * A5p2s / 25.;
+
+	double A5s2p = 4.2955e+05 + 8.5920e+05;
+	double A5d2p = (4 * 1.5709e+06 + 6 * 9.4254e+06 + 4 * 7.8548e+06) / 10.;
+	double A52p = (A5s2p + 5 * A5d2p) / 25.;
+
 	double A52 = 2.5304e+06;
-	double A52p = 3 * A52 / 4.;
-	double A52s = A52 / 4.;
+	printf("A52 = %e, A52p + A52s = %e\n", A52, A52p + A52s);
+
+	// Decays to 3
+	double A43 = 8.9860e+06;
 	double A53 = 2.2008e+06;
+
+	// Decays to 4
 	double A54 = 2.6993e+06;
 
 	Eigen::MatrixXd the_avv(NLV, NLV);
@@ -119,7 +148,7 @@ Eigen::MatrixXd HydrogenLevels::prepareCollisionMatrix(double T, double electron
 {
 	Eigen::MatrixXd Cvv = Eigen::MatrixXd::Zero(NLV, NLV);
 
-	auto setElectronCollisionRate = [&](size_t upper, size_t lower, double bigUpsilon) {
+	auto fillInElectronCollisionRate = [&](size_t upper, size_t lower, double bigUpsilon) {
 		double kT = Constant::BOLTZMAN * T;
 		// Equation 6.17 of Hazy II (6.6 Collision strengths)
 		Cvv(upper, lower) = bigUpsilon * 8.6291e-6 / gv(upper) / sqrt(T) * electronDensity;
@@ -129,7 +158,7 @@ Eigen::MatrixXd HydrogenLevels::prepareCollisionMatrix(double T, double electron
 
 	// data from 2002-anderson
 	// Electron temperatures in electron volt
-	vector<double> grid_eT_eVv = {.5, 1., 3., 5., 10., 15., 20., 25.};
+	const vector<double> grid_eT_eVv = {.5, 1., 3., 5., 10., 15., 20., 25.};
 
 	double eT_eV = Constant::BOLTZMAN * T * Constant::ERG_EV;
 
@@ -143,20 +172,20 @@ Eigen::MatrixXd HydrogenLevels::prepareCollisionMatrix(double T, double electron
 
 	// Effective collision strength
 	vector<double> bigUpsilon2s1v = {2.6e-1,  2.96e-1, 3.26e-1, 3.39e-1,
-	                               3.73e-1, 4.06e-1, 4.36e-1, 4.61e-1};
+	                                 3.73e-1, 4.06e-1, 4.36e-1, 4.61e-1};
 	double bigUpsilon2s1 = TemplatedUtils::interpolateLinear(
 	                eT_eV, grid_eT_eVv[iLeft], grid_eT_eVv[iRight], bigUpsilon2s1v[iLeft],
 	                bigUpsilon2s1v[iRight]);
 	bigUpsilon2s1 = max(bigUpsilon2s1, 0.);
-	setElectronCollisionRate(2, 0, bigUpsilon2s1);
+	fillInElectronCollisionRate(2, 0, bigUpsilon2s1);
 
 	vector<double> bigUpsilon2p1v = {4.29e-1, 5.29e-01, 8.53e-01, 1.15e00,
-	                               1.81e00, 2.35e00,  2.81e00,  3.20e00};
+	                                 1.81e00, 2.35e00,  2.81e00,  3.20e00};
 	double bigUpsilon2p1 = TemplatedUtils::interpolateLinear(
 	                eT_eV, grid_eT_eVv[iLeft], grid_eT_eVv[iRight], bigUpsilon2p1v[iLeft],
 	                bigUpsilon2p1v[iRight]);
 	bigUpsilon2p1 = max(bigUpsilon2s1, 0.);
-	setElectronCollisionRate(1, 0, bigUpsilon2p1);
+	fillInElectronCollisionRate(1, 0, bigUpsilon2p1);
 
 	// Important for two-photon continuum vs lyman is the l-changing collisions between 2s and
 	// 2p 1964-Pengelly eq 43, assuming for qnl = qnl->nl' if l can only be l=1 or l=0
