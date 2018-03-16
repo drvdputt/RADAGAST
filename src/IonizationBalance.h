19f1423a8cc5bac26081ac09ca17f3d32a103bbe
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2018-03-15 22:41:13 -0400

    Comment formatting.

--- src/IonizationBalance.h
+++ src/IonizationBalance.h
@@ -5,12 +5,12 @@
 
 namespace Ionization
 {
-/** Solves the ionization balance (in the nebular approximation, i.e. this function assumes that all
-    hydrogen is in the ground state). */
+/** Solves the ionization balance (in the nebular approximation, i.e. this function assumes that
+    all hydrogen is in the ground state). */
 double solveBalance(double nH, double T, const Spectrum& specificIntensity);
 
-/** Photoionization rate coefficient. Integrates over the spectrum. Multiply with neutral density
-    to get total rate. */
+/** Photoionization rate coefficient. Integrates over the spectrum. Multiply with neutral
+    density to get total rate. */
 double photoRateCoeff(const Spectrum& specificIntensity);
 
 /** Photoionization cross section in cm2 */
@@ -19,14 +19,14 @@ double crossSection(double frequency);
 /** Collisional ionization rate coefficient from the ground state in cm3 / s */
 double collisionalRateCoeff(double T);
 
-/** Total radiative recombination rate coefficient in cm3 / s */
+/** Total radiative recombination rate coefficient in cpm3 / s */
 double recombinationRateCoeff(double T);
 
 /** Heating due to thermalization of freed electrons */
 double heating(double nH, double f, double T, const Spectrum& specificIntensity);
 
-/** Kinetic energy lost during recombination (basically the photon energy, minus the binding energy
-    contribution. */
+/** Kinetic energy lost during recombination (basically the photon energy, minus the binding
+    energy contribution. */
 double cooling(double nH, double np, double ne, double T);
 
 const double THRESHOLD = 3.28984196e15; // Hertz;
