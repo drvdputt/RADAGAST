98a225a08d2dcb4eb3cbca399f059f5d6752a024
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2018-05-24 15:32:22 +0200

    Extra documentation for Spectrum average

--- src/Spectrum.h
+++ src/Spectrum.h
@@ -22,7 +22,9 @@ public:
 	/** Interpolates the data to the given frequency. */
 	double evaluate(double frequency) const;
 
-	/** Calculate the average of the spectrum over a given interval. */
+	/** Calculate the average of the spectrum over a given interval. The points used to
+	    perform the integration are the points on which the spectrum is discretized, with
+	    minFreq and maxFreq added extra to fix the range. */
 	double average(double minFreq, double maxFreq) const;
 
 	/** Bin this spectrum onto another frequency grid. The argument given is a list of
