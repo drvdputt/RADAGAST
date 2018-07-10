3fafeb12d2b5818a831e20915e4a85cf1d772340
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2018-07-10 16:10:58 +0200

    Recombination rate class, with subclass for openADAS data.

---
+++ src/RecombinationRate.h
@@ -0,0 +1,36 @@
+#ifndef GASMODULE_GIT_SRC_RECOMBINATIONRATE_H_
+#define GASMODULE_GIT_SRC_RECOMBINATIONRATE_H_
+
+#include "Array.h"
+#include "Table.h"
+
+#include <array>
+#include <map>
+#include <vector>
+
+class RecombinationRate
+{
+public:
+	RecombinationRate() = default;
+	virtual ~RecombinationRate() = default;
+	virtual double alpha(int n, int l, double T) const = 0;
+};
+
+class HydrogenADF48 : public RecombinationRate
+{
+public:
+	HydrogenADF48();
+	~HydrogenADF48();
+
+private:
+	void readADF48File(const std::string& path);
+
+public:
+	double alpha(int n, int l, double T) const override;
+
+private:
+	std::map<std::array<int, 2>, size_t> _nlToIndexm;
+	Array _temperaturev;
+	std::vector<std::vector<double>> _alphavv;
+};
+#endif /* GASMODULE_GIT_SRC_RECOMBINATIONRATE_H_ */
