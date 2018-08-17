7e88cd9cb960c70a91cb4bc6a80f0844734a793c
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2018-08-17 15:48:32 +0200

    Reflow comment

--- src/GasGrainInteraction.h
+++ src/GasGrainInteraction.h
@@ -8,10 +8,10 @@ class GrainInterface;
 
 namespace GasGrain
 {
-/** Implementation of the grain surface H2 formation rate recipe decribed by Cazaux & Tielens (2002,
-    2004, 2010) and summarized in Rollig et al 2013. This particular implementation returns the
-    formation rate without muliplything with nH (atomic hydrogen number density [cm-3]). Since nH *
-    rate = [cm-3 s-1], the unit of the returned rate is s-1. */
+/** Implementation of the grain surface H2 formation rate recipe decribed by Cazaux & Tielens
+    (2002, 2004, 2010) and summarized in Rollig et al 2013. This particular implementation
+    returns the formation rate without muliplything with nH (atomic hydrogen number density
+    [cm-3]). Since nH * rate = [cm-3 s-1], the unit of the returned rate is s-1. */
 double surfaceH2FormationRateCoeff(const GasModule::GrainInterface& g, double Tgas);
 
 /** Implementation of a simple gas-grain energy exchange formula. Does not depend on grain
