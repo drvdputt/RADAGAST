886e0f2e30482e1a5bc0673caa417a932dcd503a
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2018-09-11 16:54:49 +0200

    WIP test for H2Levels with radiation

--- src/core/test_H2Levels.cpp
+++ src/core/test_H2Levels.cpp
@@ -8,7 +8,7 @@
 
 TEST_CASE("H2-specific algorithm")
 {
-	H2FromFiles hff(2, 1);
+	H2FromFiles hff(15, 3);
 	H2Levels h2l(std::make_shared<const H2FromFiles>(hff));
 
 	double T = 500;
@@ -17,7 +17,7 @@ TEST_CASE("H2-specific algorithm")
 	EVector zerov = EVector::Zero(h2l.numLv());
 	Array frequencyv = Testing::generateGeometricGridv();
 
-	double epsFrac = 1e-4;
+	double epsFrac = 1e-2;
 
 	SUBCASE("no radiation, high density, should go to LTE")
 	{
@@ -40,8 +40,6 @@ TEST_CASE("H2-specific algorithm")
 			                                                   epsFrac),
 			              "level " << i << " from solver: " << s0.nv(i)
 			                       << ", from LTE:" << sLTE.nv(i));
-			std::cout << "level " << i << " from solver: " << s0.nv(i)
-			          << ", from LTE:" << sLTE.nv(i) << std::endl;
 		}
 
 		// Check if the total density is correct
@@ -66,9 +64,42 @@ TEST_CASE("H2-specific algorithm")
 		for (size_t i = 1; i < std::min<int>(5, nv.size()); i++)
 			WARN_MESSAGE(nv(i) == 0, "nv(" << i << ") = " << nv(i));
 
-		// Check if almost all density is in lowest level
+		// Check if almost all density is in lowest (two) level(s) (ortho-para)
 		double eps = 1e-12;
-		CHECK(TemplatedUtils::equalWithinTolerance(nv(0), n, eps));
+		CHECK_MESSAGE(TemplatedUtils::equalWithinTolerance(nv(0) + nv(1), n, eps),
+		              "nv(0) = " << nv(0) << " while n = " << n);
 		CHECK((nv.tail(nv.size() - 1).array() < eps).all());
 	}
+
+	// TODO: This is not working as expected. Of course, the above examples do not depends
+	// on radiation field. I need a way to check that the effect on radiation on level
+	// populations is hanled correctly.
+	// SUBCASE("Blackbody radiation, should also go to LTE?")
+	// {
+	// 	double n = 1;
+	// 	double T = 250;
+	// 	speciesNv(SpeciesIndex::ine()) = 0;
+	// 	speciesNv(SpeciesIndex::inp()) = 0;
+	// 	speciesNv(SpeciesIndex::inH()) = 0;
+	// 	speciesNv(SpeciesIndex::inH2()) = n;
+	// 	const GasStruct gas(T, speciesNv);
+	// 	Array specificIntensityv(frequencyv.size());
+	// 	for (size_t i = 0; i < frequencyv.size(); i++)
+	// 		specificIntensityv[i] = SpecialFunctions::planck(frequencyv[i], T);
+	// 	Spectrum specificIntensity(frequencyv, specificIntensityv);
+	// 	NLevel::Solution s0 = h2l.solveBalance(n, specificIntensity, zerov, zerov, gas);
+	// 	NLevel::Solution sLTE = h2l.solveLTE(n, specificIntensity, gas);
+	// 	for (size_t i = 0; i < std::min<int>(4, s0.nv.size()); i++)
+	// 	{
+	// 		CHECK_MESSAGE(TemplatedUtils::equalWithinTolerance(s0.nv(i), sLTE.nv(i),
+	// 		                                                   epsFrac),
+	// 		              "level " << i << " from solver: " << s0.nv(i)
+	// 		                       << ", from LTE:" << sLTE.nv(i));
+	// 	}
+
+	// 	// Check if the total density is correct
+	// 	double eps = 1e-15;
+	// 	CHECK(TemplatedUtils::equalWithinTolerance(s0.nv.sum(), n, eps));
+	// 	CHECK(TemplatedUtils::equalWithinTolerance(sLTE.nv.sum(), n, eps));
+	// }
 }
