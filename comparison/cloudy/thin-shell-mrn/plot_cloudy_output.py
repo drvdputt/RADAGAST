cf960647fde0cdd5e3e6ea263b1ec4d66799289f
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2019-05-29 14:53:26 +0200

    Ignore last dust column in cloudy plot

--- comparison/cloudy/thin-shell-mrn/plot_cloudy_output.py
+++ comparison/cloudy/thin-shell-mrn/plot_cloudy_output.py
@@ -7,8 +7,9 @@ output_dir.mkdir(exist_ok=True)
 
 # plot grain size distribution
 grainabundance = np.loadtxt('grainabundance.g_cm-3.out')
-x = range(1, grainabundance.shape[1])
-y = grainabundance[0, 1:]
+# first column = depth, last column = total
+x = range(grainabundance.shape[1])[1:-1]
+y = grainabundance[0, 1:-1]
 plt.figure()
 plt.semilogy(x, y, marker='+')
 plt.xlabel('grain pop number')
