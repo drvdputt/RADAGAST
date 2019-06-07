c23fe75f16791709814d3aa8d56d533cc4e4a69f
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2019-06-07 17:43:56 +0200

    Finally reasonable H2 formation ~ order of magnitude of cloudy ELRD

--- src/core/GasGrainInteraction.cpp
+++ src/core/GasGrainInteraction.cpp
@@ -8,9 +8,10 @@ double GasGrain::surfaceH2FormationRateCoeff(const GasModule::GrainInterface& gI
                                              double Tgas)
 
 {
+	// See Rollig et al. (2013) appendix C + erratum of 2002 Cazaux and Tielens paper
 	double total{0};
 
-	double thermalVelocityH{sqrt(Constant::BOLTZMAN * Tgas / Constant::HMASS_CGS)};
+	double Tgas_100 = Tgas / 100.;
 
 	size_t numPop = gInterface.numPopulations();
 	for (size_t i = 0; i < numPop; i++)
@@ -19,6 +20,20 @@ double GasGrain::surfaceH2FormationRateCoeff(const GasModule::GrainInterface& gI
 		const GrainType* grainType = pop->type();
 		const GasModule::SfcInteractionPar& surfaceParams =
 		                grainType->sfcInteractionPar();
+
+		double Es{surfaceParams._es};
+		double EHp{surfaceParams._eHp};
+		double EHc{surfaceParams._eHc};
+		double aSqrt{surfaceParams._aSqrt};
+		double F{surfaceParams._f};
+		double nu_Hc{surfaceParams._nuHc};
+
+		double EHc_Es = EHc - Es;
+		double sqrtEHp_Es = sqrt(EHp - Es);
+		double sqrtEHc_Es = sqrt(EHc_Es);
+		double sqrtEHc_Ehp = sqrt(EHc - EHp);
+		double onePlusSqrtFrac = 1. + sqrtEHc_Es / sqrtEHp_Es;
+
 		size_t numSizes = pop->numSizes();
 		for (size_t iSize = 0; iSize < numSizes; iSize++)
 		{
@@ -26,42 +41,44 @@ double GasGrain::surfaceH2FormationRateCoeff(const GasModule::GrainInterface& gI
 			double nd{pop->densityv()[iSize]};
 			double Td{pop->temperaturev()[iSize]};
 
-			// Cross section of the grain
+			// Cross section of the grain. This needs to be an integrated quantity
+			// over the bin, but lets approximate for now. TODO: integrated cross
+			// section for grains.
 			double sigmad{pop->sizev()[iSize]};
-			sigmad *= sigmad / 4.;
-
-			// Formation efficiency epsilon
-			double Es{surfaceParams._es};
-			double EHp{surfaceParams._eHp};
-			double EHc{surfaceParams._eHc};
-			double aSqrt{surfaceParams._aSqrt};
-			double F{surfaceParams._f};
-			double nu_Hc{surfaceParams._nuHc};
-			double sqrtEHp_Es = sqrt(EHp - Es);
-			double sqrtEHc_Es = sqrt(EHc - Es);
+			sigmad *= sigmad * Constant::PI;
 
 			// 1 / B
-			double oneOverB{4. * exp(Es / Td) * sqrtEHp_Es / sqrtEHc_Es +
-			                8 * sqrt(Constant::PI * Td) / (EHc - Es) *
-			                                exp(-2 * aSqrt) * exp(EHp / Td) *
-			                                sqrtEHc_Es};
-			double onePlusSqrtFrac{1 * sqrtEHc_Es / sqrtEHp_Es};
-			double oneOverKsi{1 + nu_Hc / 2. / F * exp(-1.5 * EHc / Td) *
-			                                      onePlusSqrtFrac *
-			                                      onePlusSqrtFrac};
-			// eps = (1 + B)^-1 * ksi
-			double epsilon{1 / (1 + 1 / oneOverB) / oneOverKsi};
+			double beta_alpha = 1. / (4. * exp(Es / Td) * sqrtEHp_Es / sqrtEHc_Es +
+			                          8. * sqrt(Constant::PI * Td) *
+			                                          exp(-2. * aSqrt + EHp / Td) *
+			                                          sqrtEHc_Ehp / EHc_Es);
 
-			// Sticking coefficient
-			double oneOverStick{1 + 0.04 * sqrt((Tgas + Td) / 100.) +
-			                    0.2 * Tgas / 100. + 0.08 * (Tgas * Tgas / 10000.)};
+			double xi = 1. / (1. + nu_Hc * exp(-1.5 * EHc / Td) * onePlusSqrtFrac *
+			                                       onePlusSqrtFrac / 2. / F);
+			// The minus sign in the exponential is not there in Rollig 2013! The
+			// erratum for Cazaux and Tielens (2002) has the correct version of
+			// formula 16 of CT02).
+
+			// eps = (1 + B)^-1 * ksi
+			double epsilon = xi / (1. + beta_alpha);
+
+			// Sticking coefficient (same paper, equation 20; originally from
+			// Hollenbach and Mckee (!979), equation 3.7, or Burke and Hollenbach
+			// (1979)). Annoyingly, Rollig et al states 0.04 instead of 0.4 for the
+			// first coefficient.
+			double S = 1. / (1. + 0.4 * sqrt(Tgas_100 + Td / 100.) +
+			                 0.2 * Tgas_100 + 0.08 * Tgas_100 * Tgas_100);
 
 			// factor n_d * sigma_d * epsilon_H2 * S_h
-			double contribution = nd * sigmad * epsilon / oneOverStick;
+			double contribution = nd * sigmad * epsilon * S;
 			if (!std::isnan(contribution))
 				total += contribution;
 		}
 	}
+	// Use mean particle speed of Maxwell distribution, not RMS as suggested by comment in
+	// Cloudy source code.
+	double thermalVelocityH = sqrt(8. * Constant::BOLTZMAN / Constant::PI * Tgas /
+	                               Constant::HMASS_CGS);
 	return 0.5 * thermalVelocityH * total;
 }
 
