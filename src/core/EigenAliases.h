cfbee9a08a8b6c1479a6a6a6e55b7a71989c0a95
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2019-08-23 15:08:44 +0200

    Eigen matrix of booleans might be useful

--- src/core/EigenAliases.h
+++ src/core/EigenAliases.h
@@ -4,6 +4,7 @@
 #include <Eigen/Dense>
 
 using EMatrix = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+using EMatrix_bool = Eigen::Matrix<bool, Eigen::Dynamic, Eigen::Dynamic>;
 // Row major version can be useful for interfacing with e.g. GSL
 using EMatrixRM = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 using EVector = Eigen::Matrix<double, Eigen::Dynamic, 1>;
