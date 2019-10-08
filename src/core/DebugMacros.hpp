cf479846fa2c566c2aabe82ded6db7bd2b9021b4
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2019-10-08 15:46:50 +0200

    Remove obsolete debug macros

--- src/core/DebugMacros.hpp
+++ src/core/DebugMacros.hpp
@@ -4,26 +4,16 @@
 #include <iostream>
 #include <ios>
 
-/* #define SANITY */
-
 #ifdef SILENT
 #define DEBUG(x)                                                                               \
 	do                                                                                     \
 	{                                                                                      \
 	} while (0)
 #else
-
-/* #define PRINT_LEVEL_MATRICES */
-/* #define PRINT_CHEMISTRY_MATRICES */
-/* #define REPORT_LINE_QUALITY */
-/* #define DEBUG_CONTINUUM_DATA */
-/* #define ECHO_READIN */
-
 #define DEBUG(x)                                                                               \
 	do                                                                                     \
 	{                                                                                      \
 		std::cout << std::scientific << x;                                            \
 	} while (0)
 #endif
-
 #endif /* CORE_DEBUGMACROS_H_ */
