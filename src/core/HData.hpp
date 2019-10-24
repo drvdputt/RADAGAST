505ebdbde0ec623156b8bdb88c836d45c140c166
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2019-10-24 12:01:04 +0200

    Documentation for HData

--- src/core/HData.hpp
+++ src/core/HData.hpp
@@ -7,14 +7,19 @@
 #include <array>
 
 /** Abstract class which specifies how to provide data for the hydrogen model. It inherits from
-    LevelCoefficients, so that level coefficients can be delivered in a consistent way. */
+    LevelCoefficients, so that level coefficients can be delivered in a consistent way. The
+    recombination coefficients are concretely implemented here. */
 class HData : public LevelCoefficients
 {
 public:
 	HData();
 	virtual ~HData() = default;
 
+	/** The maximum quantum number n for which data is available */
 	virtual int nMax() const = 0;
+
+	/** Get the level index for a given pair of quantum numbers n, l. If the data is not
+	    resolved on l, the l argument is ignored. */
 	virtual size_t index(int n, int l) const = 0;
 
 	/** Returns the upper and lower index (in that order) of the two photon transition as an
@@ -24,6 +29,8 @@ public:
 	    implementations for this. */
 	virtual std::array<size_t, 2> twoPhotonIndices() const = 0;
 
+	/** Get the recombination rate to each level. Multiply with ne * np to get cm-3 s-1.
+	    [cm3 s-1] */
 	EVector recombinationRatev(double T) const;
 
 private:
