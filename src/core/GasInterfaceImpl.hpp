7688be04276461ab9ea55da798a7b831fabd5ced
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2019-10-24 11:31:23 +0200

    Rewrite documentation for GasInterfaceImpl

--- src/core/GasInterfaceImpl.hpp
+++ src/core/GasInterfaceImpl.hpp
@@ -18,27 +18,32 @@ class ChemistrySolver;
     is found.
 
     When an instance is constructed, a few members which provide implementations of the various
-    processes are constructed too. Objects of the classes FreeFree, FreeBound and Chemistry are
-    created, which decribe respectively the Brehmsstrahlung, the recombination continuum and the
-    chemical network. During their construction, they each read the data they need to perform
-    their functions.
+    processes are constructed too. FreeFree, FreeBound and Chemistry objects are created, which
+    decribe respectively the Brehmsstrahlung, the recombination continuum and the chemical
+    network. During their construction, they each read the data they need to perform their
+    functions.
 
-    Another important member is the SpeciesModelManager, which contains all constant data for
-    specialized species models (currently H and H2). I also allows this class to create HModel
-    and H2Model objects, which will be given to a GasSolution object to store all the details of
-    H and H2.
+    The main methods of this class return a GasSolution object. At construction, the GasSolution
+    takes ownership of HModel and H2Model objects created using the SpeciesModelManager, and
+    receives (const) references to the FreeFree and FreeBound instances contained here. From the
+    GasSolution, full details about the solution can be obtained (see its documentation).
 
-    The main methods of this class return a GasSolution object, which is given a callback
-    pointer. Any quantities which depend on the specifics of the final equilibrium will be
-    calculated in that class. This class has a handful of extra functions, to give the
-    GasSolution access to some of the functionality of FreeFree and FreeBound. */
+    From a GasSolution object, more lightweight GasState objects can also be obtained. A few
+    convenience functions are provided in this class to skip having to make a GasSolution object
+    (an intermediary GasSolution still appears though). Only using GasState objects drastically
+    reduces the amount of includes needed. The GasState objects can be given to a few functions
+    in this class to calculate the emissivity and opacity.
+
+    All the functions in this class are reentrant, which means that they can safely be called
+    simultaneously by different threads. All the non-constant variables are contained in the
+    GasSolution objects, of which multiple can exist at the same time. */
 class GasInterfaceImpl
 {
 public:
 	/** Creates an instance of the gas module. Multiple frequency grids are used, which need
 	    to be specified by the user. Some configuration options in the form of strings are
-	    also provided. Currently, they only influence the settings of the H and H2
-	    models. */
+	    also provided. Currently, they only influence the settings of the H and H2 models,
+	    see SpeciesModelManager.. */
 	GasInterfaceImpl(const Array& iFrequencyv, const Array& oFrequencyv,
 	                 const Array& eFrequencyv, const std::string& atomChoice = "",
 	                 const std::string& moleculeChoice = "");
@@ -54,13 +59,11 @@ public:
 	/** The grid on which the emissivity is calculated. */
 	std::valarray<double> eFrequencyv() const { return _eFrequencyv; }
 
-	// GASSTATE TOOLS (minimal information, works with GasSolution under the hood) //
-
-	/** The main way to run the code for a cell. A minimal set of results is stored in the
-	    given GasState object. The exact contents of the GasState are not known to the user.
-	    Through this interface, the variable information contained in the gas state can be
-	    combined with other constants and functions to retrieve the opacity and emissivity
-	    of the gas.
+	/** The most convenient way to run the code for a cell. A minimal set of results is
+	    stored in the given GasState object. The exact contents of the GasState are not
+	    known to the user. Through this interface, the variable information contained in the
+	    gas state can be combined with other constants and functions to retrieve the opacity
+	    and emissivity of the gas.
 
 	    The other arguments reflect the physical conditions in the cell for which a gas
 	    state is being calculated. The density of hydrogen nuclei, n; the ambient radiation
@@ -71,15 +74,16 @@ public:
 	    to construct one of these objects. The absorption efficiency of the grains currently
 	    needs to be tabulated for the frequencies contained in iFrequencyv.
 
-	    Note that the temperatures in GrainInterface can be modified, to take into account
-	    the effect of gas-grain collisions. */
+	    Note that the GrainInterface instance can be modified; the temperatures are
+	    recalculated to take into account the effect of gas-grain collisions. */
 	void updateGasState(GasModule::GasState&, double n,
 	                    const std::valarray<double>& specificIntensityv,
 	                    GasModule::GrainInterface& gri, GasDiagnostics* gd = nullptr) const;
 
 	/** Does the same as the above, but without an input radiation field. Instead, a
-	    blackbody of the given temperature is used to calculate GasState. It is recommended
-	    to apply this function to all gas states before starting a simulation. */
+	    blackbody of the given temperature is used to calculate the GasState. It is
+	    recommended to apply this function to all gas states before starting a
+	    simulation. */
 	void initializeGasState(GasModule::GasState&, double n, double T,
 	                        GasModule::GrainInterface&, GasDiagnostics* gd = nullptr) const;
 
@@ -87,50 +91,61 @@ public:
 	    s-1 Hz-1 sr-1 = 0.1 J m-3 s-1 Hz-1 sr-1). [W m-3 Hz-1 sr-1] */
 	double emissivity_SI(const GasModule::GasState& gs, size_t iFreq) const;
 
-	/** Returns the total opacity in SI units (converted from cm-1 = 100 * m-1). [m-1]*/
+	/** The total opacity in SI units (converted from cm-1 = 100 * m-1). [m-1] */
 	double opacity_SI(const GasModule::GasState& gs, size_t iFreq) const;
 
-	// GASSOLUTION TOOLS (full information) //
-
 	/** This convenience function runs solveTemperature for a blackbody radiation field. The
-	    final temperature should be close to the given blackbody temperature (?) */
+	    final temperature is usually close to the given color temperature T. */
 	GasSolution solveInitialGuess(double n, double T, GasModule::GrainInterface&) const;
 
-	/** Find a fully self-consistent solution, given a total hydrogen density n and a
-	    Spectrum object describing the radiation field in specific intensity units [erg s-1
-	    sr-1 cm-2]. */
+	/** Find a fully self-consistent solution and temperature, given a total hydrogen
+	    density n and a Spectrum object describing the radiation field in specific intensity
+	    units [erg s-1 sr-1 cm-2]. */
 	GasSolution solveTemperature(double n, const Spectrum& specificIntensity,
 	                             GasModule::GrainInterface&) const;
 
-	/** Calculates all the densities for a fixed temperature. */
+	/** Find a fully self-consistent solution for a fixed temperature (heating/cooling will
+	    be out of equilibrium). */
 	GasSolution solveDensities(double n, double T, const Spectrum& specificIntensity,
 	                           GasModule::GrainInterface&,
 	                           double h2FormationOverride = -1) const;
 
-	/** Recalculate the densities for a GasSolution object, with a different temperature. Is
-	    repeatedly called by this class until equilibrium is found. */
+	/** Recalculate the densities for a GasSolution object. If startFromCurrent is true, the
+	    current contents of the GasSolution are used as an initial guess if the given
+	    temperature doesn't differ too much (< factor 2) from the previous temperature.
+	    Otherwise, an initial guess is made for the chemistry, where the ionized fraction is
+	    based on the radiation field, and the initial molecular fraction is 0.1. */
 	void solveDensities(GasSolution&, double n, double T, const Spectrum& specificIntensity,
 	                    GasModule::GrainInterface&, bool startFromCurrent = false,
 	                    double h2FormationOverride = -1) const;
 
-	/** TODO: rework old code */
+	/** NOT IMPLEMENTED. Solve for a fixed temperature, forcing H2 to zero. Useful for
+	    ionization-only tests. */
 	GasSolution solveDensitiesNoH2(double n, double T, const Spectrum& specificIntensity,
 	                               GasModule::GrainInterface&) const;
 
 	/** Calculate several extra contributions to the heating of the grains (collisions
-	    (Draine and Bertoldi 1996), H2 formation on the surface (Takahashi 2001). Passing
-	    some intermediary results of either the H2 level calculation or the grain
-	    photoelectric effect calculation might help in speeding up this computation, as well
-	    as using caching for the radiation emitted by the grains.
-
+	    (Draine and Bertoldi 1996) and heat of H2 formation on the surface (Takahashi
+	    2001)).
+ 
 	    Leaving this here for now, instead of moving it to GasSolution. GasSolution is not
 	    the owner of the GrainInterface object, and it would be weird to modify it from
-	    there. */
+	    there.
+
+	    If this takes up too much time, there is still much room for optimization (such as
+	    storing intermediate results of the photoelectric heating calculation inside the
+	    GasSolution, or implementing caching mechanisms for the former). */
 	void updateGrainTemps(const GasSolution& s, GasModule::GrainInterface& g) const;
 
 private:
+	/** Construct a new GasSolution model, passing all the necessary (references to)
+	    objects. The SpeciesModelManager is used to create the HModel and H2Model. */
 	GasSolution makeGasSolution(const Spectrum& specificIntensity,
 	                            const GasModule::GrainInterface&) const;
+
+	/** Create a species vector which is zero everywhere, except for e-, p+, H, and H2.
+	    Arguments: the total amount of H nuclei n, the ionized to total fraction (np / n),
+	    the molecular to neutral fraction (2 * nH2 / (nH + 2 * nH2)). */
 	EVector guessSpeciesNv(double n, double ionToTotalFrac,
 	                       double moleculeToNeutralFrac) const;
 
