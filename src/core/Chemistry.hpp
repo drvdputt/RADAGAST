5c51229ddc723d641e17e36ed20ee5ef1a284c90
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2019-10-28 12:03:55 +0100

    Move documentation to Chemistry and delete ChemicalNetwork etc.

--- src/core/Chemistry.hpp
+++ src/core/Chemistry.hpp
@@ -7,10 +7,18 @@
 #include <map>
 #include <vector>
 
+/** This class can store a list of reactions, and solve the chemical equilibrium based on the
+    coefficients of the reactions and their reaction rates. */
 class Chemistry
 {
 public:
 	virtual ~Chemistry() = default;
+
+	/** Function to provide a clear syntax for adding reactions in the setup of the chemical
+	    network. Each reaction is given a number, and the reaction is added to the reaction
+	    index using the given name as a key. Subclasses typically implement a constructor
+	    which calls addReaction and prepareCoefficients. Manually adding an extra reaction
+	    is still possible afterwards, but make sure to call prepareCoefficients again. */
 	void addReaction(const std::string& reactionName,
 	                 const std::vector<std::string>& reactantNamev,
 	                 const Array& reactantStoichv,
@@ -20,26 +28,43 @@ public:
 	/** This should be called after reactions have been added */
 	void prepareCoefficients();
 
+	/** Look up the index of a reaction, based on the name that was given in to addReaction. */
 	int reactionIndex(const std::string& reactionName) const;
+
+	/** The number of reactions. This determines the size of the reaction rate vector */
 	int numReactions() const { return _reactionv.size(); }
+
+	/** The number of species. This determines the size of the species density vector */
 	int numSpecies() const { return _numSpecies; }
 
+	/** Solves the chemical network given a certain rate coefficient vector (indexed on the
+	    reactions). An initial value n0v can be given. A vector containing updated densities
+	    is returned. */
 	EVector solveBalance(const EVector& rateCoeffv, const EVector& n0v) const;
+
+	/** Evaluate the rate of change for each species [cm-3 s-1]. */
 	EVector evaluateFv(const EVector& nv, const EVector& rateCoeffv) const;
+
+	/** Evaluate the Jacobian of Fv [s-1]. Every column j is the derivative of Fv towards
+	    n_j. */
 	EMatrix evaluateJvv(const EVector& nv, const EVector& rateCoeffv) const;
 
 private:
-	// help setting the members
+	// Turn list of reactions into coefficient matrices
 	EMatrix makeReactantStoichvv() const;
 	EMatrix makeProductStoichvv() const;
-	EMatrix makeConservationCoeffvv() const;
 
+	/** Solve the chemistry by evolving the system until equilibrium. */
 	EVector solveTimeDep(const EVector& rateCoeffv, const EVector& n0v) const;
-	double densityProduct(const EVector& nv, size_t r) const;
-	double densityProductDerivative(const EVector& nv, int r, int j) const;
 
-	// I'm currently combining the members from ChemistrySolver and ChemicalNetwork. Should
-	// refactor out the redundancies later, after tests have been written/ported.
+	/** Calculate the density factor needed to calculate the speed of reaction r. Formula:
+	    Product_i n_i ^ Rir, where n_i are the elements of nv, and Rir = _rStoichvv(i,
+	    r). */
+	double densityProduct(const EVector& nv, size_t r) const;
+
+	/** Calculate the derivative of the density product for reaction r with respect to the
+	    density j. Formula: (Rjr - 1) * n_j^{Rjr - 1} * Product_{i != j} n_i ^ Rir */
+	double densityProductDerivative(const EVector& nv, int r, int j) const;
 
 	typedef struct Reaction
 	{
--- src/core/SimpleHChemistry.hpp
+++ src/core/SimpleHChemistry.hpp
@@ -9,6 +9,14 @@ class SimpleHChemistry : public Chemistry
 {
 public:
 	SimpleHChemistry();
+
+	/** Calculate the rate coefficients for each reaction. Multiplying with the right
+	    density factors of the reactants will give a total rate [cm-3 s-1]. The unit of
+	    these coefficients is hence [cm(-3+n) s-1] where n is usually the number of reactant
+	    particles. Since the calculation of the H2 formation and destruction rate are
+	    complicated, the are expected to be calculated somewhere else, and passed as
+	    arguments here. This function simply fills them in into the right spot of the
+	    k-vector. */
 	EVector rateCoeffv(double T, const Spectrum& specificIntensity,
 	                   double kDissFromH2Levels, double kH2FormationGrain) const;
 };
