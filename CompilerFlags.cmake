faff8fbc78359472f5f1d622faab6b8f533e16f2
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2019-11-25 18:09:42 +0100

    Delete compilerflags file, use add_compile_options instead

--- CompilerFlags.cmake
+++
@@ -1,4 +0,0 @@
-set_property(TARGET ${TARGET} PROPERTY CXX_STANDARD 14)
-
-target_compile_options(${TARGET} PRIVATE -Wshadow -Wall -Wextra -Wno-missing-braces -Wno-sign-compare -Werror=return-type -pedantic)
-
