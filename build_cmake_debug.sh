d32cdf092e6adbd885b182e98e1ff0b6e98d82d2
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2020-01-21 17:29:52 +0100

    Set build type on build script command line

--- build_cmake_debug.sh
+++
@@ -1,9 +0,0 @@
-SRC_DIR=$(pwd)
-NJOB=${1:-1} # set to 1 if 1st command line argument is not given
-BUILD_DIR=../cmake_debug
-mkdir $BUILD_DIR
-cd $BUILD_DIR
-
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES $SRC_DIR
-ln -sf $BUILD_DIR/compile_commands.json $SRC_DIR
-make -j$NJOB
--- build_cmake_release.sh
+++ build_cmake_release.sh
@@ -1,9 +1,20 @@
 SRC_DIR=$(pwd)
 NJOB=${1:-1} # set to 1 if 1st command line argument is not given
-BUILD_DIR=../cmake_release
+BT=${2:-Release} # set to Release if argument is not given
+
+if [[ $BT == "Debug" ]]
+then
+    BUILD_DIR=../cmake_debug
+elif [[ $BT == "Release" ]]
+then
+    BUILD_DIR=../cmake_release
+else
+    BUILD_DIR=../cmake_$BT
+fi
+
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_INSTALL_PREFIX=$BUILD_DIR $SRC_DIR
+cmake -DCMAKE_BUILD_TYPE=$BT -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_INSTALL_PREFIX=$BUILD_DIR $SRC_DIR
 ln -sf $BUILD_DIR/compile_commands.json $SRC_DIR
 make -j$NJOB
