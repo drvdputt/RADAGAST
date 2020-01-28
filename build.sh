42219c92fcdac1c3c0184b5ad37c6876ceb004b1
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2020-01-28 11:38:55 +0100

    Clean up build script some more

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@ SRC_DIR=$(pwd)
 NJOB=${1:-1} # set to 1 if 1st command line argument is not given
 BT=${2:-Release} # set to Release if argument is not given
 
-if [[ $BT == "Debug" ]]
-then
+if [[ $BT == "Debug" ]]; then
     BUILD_DIR=../cmake_debug
-elif [[ $BT == "Release" ]]
-then
+elif [[ $BT == "Release" ]]; then
     BUILD_DIR=../cmake_release
 else
     BUILD_DIR=../cmake_$BT
 fi
 
-mkdir $BUILD_DIR
+if [[ ! -d $BUILD_DIR ]]; then
+    mkdir $BUILD_DIR
+fi
 cd $BUILD_DIR
 
 cmake -DCMAKE_BUILD_TYPE=$BT -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_INSTALL_PREFIX=$BUILD_DIR $SRC_DIR
