cefe80a8c7eb6da4e1138fe94b7ef06794ebb17b
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2020-02-06 13:41:44 +0100

    Add address sanitizer flags to debug build

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ BT=${2:-Release} # set to Release if argument is not given
 
 if [[ $BT == "Debug" ]]; then
     BUILD_DIR=../cmake_debug
+    EXTRA=-DCMAKE_CXX_FLAGS="-fsanitize=address -g" -DCMAKE_C_FLAGS="-fsanitize=address -g" -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address" -DCMAKE_MODULE_LINKER_FLAGS="-fsanitize=address"
 elif [[ $BT == "Release" ]]; then
     BUILD_DIR=../cmake_release
 else
@@ -17,7 +18,10 @@ fi
 cd $BUILD_DIR
 
 # Explicitly unset EIGEN_DIR here, to test find_path
-cmake $SRC_DIR -DCMAKE_BUILD_TYPE=$BT -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -UEIGEN_DIR
+cmake $SRC_DIR \
+      $EXTRA \
+      -DCMAKE_BUILD_TYPE=$BT -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -UEIGEN_DIR
+
 ln -sf $BUILD_DIR/compile_commands.json $SRC_DIR
 make -j$NJOB
 
