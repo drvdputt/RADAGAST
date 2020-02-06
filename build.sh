ddf26135f5c674c8f3674445fa6db83b7d55567a
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2020-02-06 14:25:54 +0100

    Slight restructure in build script

--- build.sh
+++ build.sh
@@ -18,11 +18,11 @@ fi
 cd $BUILD_DIR
 
 # Explicitly unset EIGEN_DIR here, to test find_path
-cmake $SRC_DIR \
+cmake $SRC_DIR -DCMAKE_BUILD_TYPE=$BT \
       $EXTRA \
-      -DCMAKE_BUILD_TYPE=$BT -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -UEIGEN_DIR
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -UEIGEN_DIR
 
-ln -sf $BUILD_DIR/compile_commands.json $SRC_DIR
 make -j$NJOB
+ln -sf $BUILD_DIR/compile_commands.json $SRC_DIR
 
 make install
